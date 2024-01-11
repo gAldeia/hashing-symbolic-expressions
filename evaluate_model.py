8448398a593b7deff91297801a08cb8b5bc84964
Guilherme Aldeia <guilherme.seidyo@gmail.com>
2024-01-11 18:32:07 -0300

    Experiments

---
+++ evaluate_model.py
@@ -0,0 +1,181 @@
+import argparse
+import importlib
+import json
+import time
+from datetime import datetime
+
+import pandas as pd
+import numpy as np
+from pmlb import fetch_data
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_squared_error as mse
+from  models.nsga2.deap_utils import get_complexity
+
+import warnings
+warnings.filterwarnings("ignore", category=RuntimeWarning)
+
+
+def read_data(dataset, random_state, data_dir):
+    X, y = None, None
+    if dataset in ['d_airfoil','d_concrete','d_enc','d_enh',
+                   'd_housing','d_tower','d_uball5d','d_yacht']:
+        
+        data = pd.read_csv(f"{data_dir}/{dataset}.txt", sep=',')
+        X, y = data.drop('label', axis=1).values, data['label'].values
+
+    else: # We need to handle pmlb differently
+        try:
+            X, y = fetch_data(dataset, return_X_y=True, local_cache_dir=data_dir)
+        except Exception as e:
+            raise e
+        
+    if X is y and y is None:
+        raise Exception("Unknown dataset")
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, random_state=random_state)
+    
+    return X_train, X_test, y_train, y_test 
+
+
+def save_evolution(estimator, name, dataset, random_state, rdir, repeat):
+    logbook = pd.DataFrame(columns=['gen', 'evals', 'n_simplifications', 'n_new_hashes'] + 
+                            [f"{stat} {partition} {objective}"
+                             for stat in ['avg', 'med', 'std', 'min', 'max']
+                             for partition in ['train', 'val']
+                             for objective in estimator.objectives])
+    
+    for item in estimator.logbook_:
+        logbook.loc[item['gen']] = (
+            item['gen'], item['evals'], item['n_simplifications'], item['n_new_hashes'],
+            *item['avg'], *item['med'], *item['std'], *item['min'], *item['max']
+        )
+
+    logbook_filename = rdir + '_'.join([dataset, 
+                                        name, 
+                                        str(repeat), 
+                                        str(random_state),
+                                        "evolution"]) + '.csv'
+        
+    pd.DataFrame.from_dict({col: logbook[col] for col in logbook.columns}
+    ).to_csv(logbook_filename, index=False)
+
+
+def evaluate_model(
+    estimator, name, dataset, random_state, rdir, repeat, data_dir='./'):
+    """Evaluates estimator by training and predicting on the dataset."""
+
+    X_train, X_test, y_train, y_test = read_data(dataset,random_state,data_dir)
+    
+    # set random states
+    if hasattr(estimator, 'random_state'):
+        estimator.random_state = random_state
+    elif hasattr(estimator, 'seed'):
+        estimator.seed = random_state
+    
+    print('algorithm:',algorithm.name,algorithm.reg)
+    print('fitting to all data...')
+    
+    start_time = time.time()
+    estimator.fit(X_train,y_train)
+    end_time = time.time() - start_time
+    
+    if hasattr(estimator, 'logbook_') and estimator.logbook_ is not None:
+        save_evolution(estimator,name,dataset,random_state,rdir,repeat)
+
+    if "NSGA2" in name:
+        model      = str(estimator.best_estimator_).replace("ARG", "x_")
+        size       = len(estimator.best_estimator_)
+        complexity = get_complexity(estimator.best_estimator_)
+        depth      = estimator.best_estimator_.height
+    else: # Assuming the only other model is the vanilla NSGA2
+        model      = ""
+        size       = 0
+        complexity = size
+        depth      = 0
+
+    print(f'model {model}; size {size}, complexity {complexity}, depth {depth}')
+
+    # get scores
+    results = {}
+
+    # Metadata
+    results['model']        = name
+    results['dataset']      = dataset
+    results['RunID']        = repeat
+    results['random_state'] = random_state
+    results['time']         = end_time
+    results['date']         = datetime.today().strftime('%m-%d-%Y %H:%M:%S')
+
+    # metrics
+    for metric, fn, (data_X, data_y) in [
+        ('train_r2',  r2_score, (X_train, y_train)),
+        ('test_r2',   r2_score, (X_test,  y_test )),
+        ('train_mse', mse,      (X_train, y_train)),
+        ('test_mse',  mse,      (X_test,  y_test )),
+    ]:
+        score = np.nan
+        try:
+            score = fn(estimator.predict(data_X), data_y)
+        except ValueError:
+            print(f"(Failed to calculate {metric} score for {name}")
+
+        results[metric] = score
+
+    # Model
+    results['representation'] = model
+    results['size']           = size
+    results['complexity']     = complexity
+    results['depth']          = depth
+
+    print('results:',results)
+    filename = (rdir + '_'.join([dataset, 
+                            name, 
+                            str(repeat),
+                            str(random_state),
+                            "result"]) + '.json')
+    
+    with open(filename, 'w') as out:
+        json.dump(results, out, indent=4)
+
+
+################################################################################
+# main entry point
+################################################################################
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+            description="Evaluate a method on a dataset.", add_help=True)
+    
+    parser.add_argument('-ml', action='store', dest='ALG', default=None,
+            type=str, 
+            help='Name of estimator (with matching file in methods/)')
+    
+    parser.add_argument('-rdir', action='store', dest='RDIR',default=None,
+            type=str, help='Name of save file')
+    
+    parser.add_argument('-seed', action='store', dest='RANDOM_STATE',
+            default=None, type=int, help='Seed / trial')
+    
+    parser.add_argument('-repeat', action='store', dest='REPEAT',
+            default=1, type=int, help='repetition number')
+    
+    parser.add_argument('-dataset', action='store', dest='DATASET',
+            default=None, type=str, help='endpoint name')
+    
+    parser.add_argument('-datadir', action='store', dest='DDIR',default='./',
+            type=str, help='input data directory')
+
+    args = parser.parse_args()
+    print(args)
+    
+    # import algorithm 
+    print('import from','models.'+args.ALG)
+    algorithm = importlib.__import__('models.'+args.ALG,globals(),locals(),
+                                     ['reg','name'])
+
+    evaluate_model(algorithm.reg, algorithm.name, 
+                   args.DATASET, args.RANDOM_STATE, args.RDIR, args.REPEAT,
+                   data_dir = args.DDIR)
+    
---
+++ submit_jobs.py
@@ -0,0 +1,225 @@
+import os                                                                       
+import sys                                                                      
+import argparse
+import itertools as it
+
+import subprocess
+from joblib import Parallel, delayed
+
+if __name__ == '__main__':
+    # Setting up the parser
+    parser = argparse.ArgumentParser(
+        description="An analyst for quick ML applications.", add_help=True)
+
+    # Configuring the experiments ----------------------------------------------
+    parser.add_argument('-experiment',action='store',dest='EXPERIMENT', type=str,              
+                        default="evaluate_model",help=".py experiment file")
+    parser.add_argument('-dataset',action='store',dest='DATASET',type=str,              
+                        default=('d_airfoil,d_concrete,d_enc,d_enh,'
+                                 'd_housing,d_tower,d_uball5d,d_yacht'))
+    parser.add_argument('-repeats',action='store',dest='REPEATS', type=int,              
+                        default=1)
+    parser.add_argument('-repeat_number',action='store',dest='REPEAT_N', 
+                        type=int, default=-1)
+    parser.add_argument('-n_trials',action='store',dest='N_TRIALS',default=1,  
+                        type=int, help='Number of trials to run')         
+    parser.add_argument('-models',action='store',dest='MODELS', type=str,              
+                        default=('BrushClassifier,'
+                                'DecisionTree,'
+                                'BrushClassifier,'
+                                'RandomForest,'
+                                'GaussianNaiveBayes,'
+                                'LogisticRegression_L1,'
+                                'LogisticRegression_L2'
+                                ))
+    parser.add_argument('--long',action='store_true',dest='LONG', default=False)
+    parser.add_argument('-seeds',action='store',type=str,dest='SEEDS',          
+            default='14724,24284,31658,6933,1318,16695,27690,8233,24481,6832,'  
+                    '13352,4866,12669,12092,15860,19863,6654,10197,29756,14289,'        
+                    '4719,12498,29198,10132,28699,32400,18313,26311,9540,20300,'        
+                    '6126,5740,20404,9675,22727,25349,9296,22571,2917,21353,'           
+                    '871,21924,30132,10102,29759,8653,18998,7376,9271,9292')
+    parser.add_argument('-results',action='store',dest='RDIR',                  
+                        default='results',type=str,help='Results directory')  
+    parser.add_argument('-data-dir',action='store',dest='DDIR',                  
+                        default='./',type=str,help='Input data directory')     
+    
+    # Running locally or submitting to a cluster -------------------------------
+    parser.add_argument('--local', action='store_true', dest='LOCAL', 
+                        default=False, help='Run locally instead of on HPC')
+    parser.add_argument('--dryrun', action='store_true', default=False, 
+                        help='Just print fn calls')  
+    
+    parser.add_argument('--slurm',action='store_true',dest='SLURM',default=False, 
+                        help='Run on a SLURM scheduler as opposed to on LPC')
+    parser.add_argument('-A', action='store', dest='A', default='plgsrbench', 
+                        help='SLURM account')
+    parser.add_argument('-n_jobs',action='store',dest='N_JOBS',default=1,          
+                        type=int, help='Number of parallel threads to use per job')              
+    parser.add_argument('-m',action='store',dest='M',default=8000,type=int,        
+                        help='LSF memory request and limit (MB)')
+    parser.add_argument('-time', action='store', dest='time', 
+                        default='12:00:00', type=str, help='time in HR:MN:SS')
+    parser.add_argument('-max_jobs', action='store', default=3500, 
+                        type=int, help='Maximum size of job array (for slurm)')
+    parser.add_argument('-q', action='store', dest='QUEUE',
+                        default=None, type=str, help='queue name')
+    
+    # Parsing and extracting experiment settings -------------------------------
+    args = parser.parse_args()
+    
+    n_trials = len(args.SEEDS) if args.N_TRIALS < 1 else args.N_TRIALS
+    
+    print('EXPERIMENT: ', args.EXPERIMENT)
+    print('n_trials: ', n_trials)
+    print('n_jobs: ', args.N_JOBS)
+
+    seeds = args.SEEDS.split(',')[:n_trials]                          
+    
+    print('using these seeds:',seeds)
+    print('for # of repeats:',args.REPEATS)
+
+    models = args.MODELS.split(',')
+    print('for models:',models)
+    
+    if args.REPEAT_N != -1:
+        repeats = [args.REPEAT_N]
+        if args.REPEATS != 1:
+            raise ValueError('if -repeat_number is specified, '
+                    '-repeats must be 1')
+    else:
+        repeats = range(args.REPEATS)
+
+    datasets = args.DATASET.split(',')
+    print('and these datasets:',datasets)                                        
+
+    if not args.LOCAL and args.QUEUE is None:
+        if args.SLURM: 
+            print('setting queue to bch-compute')
+            args.QUEUE = 'bch-compute'
+        else:
+            args.QUEUE = 'mgh'
+                  
+    input("Press Enter to continue...")
+
+    # Generating the jobs ------------------------------------------------------
+    all_commands = []
+    job_info = [] 
+    for repeat, dataset, ml in it.product(repeats, datasets, models):
+        filepath = '/'.join([args.RDIR,dataset,ml]) + '/'
+
+        if not os.path.exists(filepath):
+            print('WARNING: creating path',filepath)
+            os.makedirs(filepath)
+        
+        for seed in seeds:
+            random_state = seed
+            all_commands.append( #Defining how we'll call the script
+                'python {CURRDIR}/{EXPERIMENT}.py '
+                ' -ml {ML}'
+                ' -dataset {DATASET}'
+                ' -seed {RS}'
+                ' -rdir {RDIR}'
+                ' -repeat {REPEAT}'
+                ' -datadir {DDIR}'.format(
+                    CURRDIR=os.path.abspath(os.getcwd()),
+                    EXPERIMENT=args.EXPERIMENT,
+                    ML=ml,
+                    DATASET=dataset,
+                    RS=random_state,
+                    RDIR=filepath,
+                    REPEAT=repeat,
+                    DDIR=args.DDIR
+                )
+            )  
+                        
+            job_info.append({
+                'ml':ml,
+                'dataset':dataset,
+                'repeat':repeat,
+                'results_path':filepath,
+                'seed':random_state
+                })
+
+    # Submiting jobs -----------------------------------------------------------
+    print('\n'.join(all_commands))
+    if args.dryrun:
+        exit()
+    if args.LOCAL:   # run locally
+        Parallel(n_jobs=args.N_JOBS)(
+            delayed(os.system)(run_cmd) for run_cmd in all_commands)
+            # delayed(print)(run_cmd) for run_cmd in all_commands)
+    else:
+        if args.SLURM:
+            # write a jobarray file to read commans from
+            jobarrayfile = 'jobfiles/joblist.txt'
+            for i, run_cmd in enumerate(all_commands):
+                mode='w' if i == 0 else 'a'
+                with open(jobarrayfile,mode) as f:
+                    f.write(f'{run_cmd}\n')
+
+            # job_name = '_'.join([f'{job_info[i][x]}' for x in
+            #                      ['ml','dataset','seed']])
+            job_name='car-t'
+            job_file = f'jobfiles/{job_name}'
+            out_file = job_info[i]['results_path'] + job_name + '_%J.out'
+            # error_file = out_file[:-4] + '.err'
+
+            mem = args.M
+            if len(all_commands)>args.max_jobs:
+                joblimit = f'%{args.max_jobs}'
+            else:
+                joblimit=''
+
+            batch_script = (
+f"""#!/usr/bin/bash 
+#SBATCH --output={args.RDIR}/cart_%A_%a.txt 
+#SBATCH --job-name={job_name} 
+#SBATCH --partition={args.QUEUE} 
+#SBATCH --cpus-per-task={args.N_JOBS} 
+#SBATCH --time={args.time}
+#SBATCH --mem-per-cpu={mem} 
+#SBATCH --array=0-{len(all_commands)}
+
+row="$SLURM_ARRAY_TASK_ID"
+declare -i row
+row+=1
+echo "head -$row {jobarrayfile} | tail -n +$row"
+cmd=$(head -$row {jobarrayfile} | tail -n +$row)
+echo $CONDA_EXE run -n cart $cmd
+$CONDA_EXE run -n cart $cmd
+"""
+            )
+
+            with open(job_file,'w') as f:
+                f.write(batch_script)
+
+            print(job_file,':')
+            print(batch_script)
+            sbatch_response = subprocess.check_output(
+                [f'sbatch {job_file}'], shell=True).decode()
+            print(sbatch_response)
+        else: #bsub
+            for i, run_cmd in enumerate(all_commands):
+                job_name = '_'.join([f'{job_info[i][x]}' for x in
+                                     ['ml','dataset','seed']])
+                job_file = f'jobfiles/{job_name}'
+                out_file = job_info[i]['results_path'] + job_name + '_%J.out'
+                # error_file = out_file[:-4] + '.err'
+
+                mem = args.M
+                bsub_cmd = ('bsub -o {OUT_FILE} -n {N_CORES} -J {JOB_NAME} '
+                            '-q {QUEUE} -R "span[hosts=1] rusage[mem={M}]" '
+                            '-M {M} ').format(
+                    OUT_FILE=out_file,
+                    JOB_NAME=job_name,
+                    QUEUE=args.QUEUE,
+                    N_CORES=args.N_JOBS,
+                    M=mem
+                )
+
+                bsub_cmd += '"' + run_cmd + '"'
+                print(bsub_cmd)
+                os.system(bsub_cmd)     # submit jobs
+
+    print('### Finished submitting',len(all_commands),'jobs. ###')
