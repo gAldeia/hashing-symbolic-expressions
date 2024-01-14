cffb287ac3659f59639285887a3272672d91080f
Guilherme Aldeia <guilherme.seidyo@gmail.com>
2024-01-14 19:53:53 -0300

    New dependencies. new models with varying tolerance

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 models=("SimplifierBottomUp,SimplifierTopDown,Vanilla")
+models=("SimplifierTopDown1e-0,SimplifierTopDown1e-1,SimplifierTopDown1e-2,SimplifierTopDown1e-4")
 
 for model in "${models[@]}"
 do
     # -repeats: runs per seed. -n_trials: number of different seeds
-    python submit_jobs.py -repeats 1 -n_trials 30 -models "$model" -n_jobs 12 -data-dir ./data/lexicase_paper --local # --dryrun # 
+    python submit_jobs.py -repeats 1 -n_trials 30 -models "$model" -n_jobs 9 -data-dir ./data/lexicase_paper --local # --dryrun # 
 done
