988afff1e65acbf56e9a8b80f732483099350c69
gAldeia <guilherme.seidyo@gmail.com>
2024-01-25 22:08:28 -0500

    Just to keep this up to date

--- run.sh
+++ run.sh
@@ -2,24 +2,27 @@
 
 models=()
 
-#models+=("Vanilla")
+models+=("Vanilla,SimplifierBottomUp,SimplifierTopDown")
+models+=("SimplifierBottomUpOnlyLast,SimplifierTopDownOnlyLast")
 
-models+=("SimplifierBottomUp")
-models+=("SimplifierTopDown")
+# models+=("Vanilla")
 
-models+=("SimplifierBottomUpOnlyLast")
-models+=("SimplifierTopDownOnlyLast")
+# models+=("SimplifierBottomUp")
+# models+=("SimplifierTopDown")
 
-models+=("SimplifierTopDown1e-0,SimplifierTopDown1e-1")
-models+=("SimplifierTopDown1e-2,SimplifierTopDown1e-4")
-models+=("SimplifierTopDown1e-6,SimplifierTopDown1e-10")
+# models+=("SimplifierBottomUpOnlyLast")
+# models+=("SimplifierTopDownOnlyLast")
 
-models+=("SimplifierBottomUp1e-0,SimplifierBottomUp1e-1")
-models+=("SimplifierBottomUp1e-2,SimplifierBottomUp1e-4")
-models+=("SimplifierBottomUp1e-6,SimplifierBottomUp1e-10")
+# models+=("SimplifierTopDown1e-0,SimplifierTopDown1e-1")
+# models+=("SimplifierTopDown1e-2,SimplifierTopDown1e-4")
+# models+=("SimplifierTopDown1e-6,SimplifierTopDown1e-10")
+
+# models+=("SimplifierBottomUp1e-0,SimplifierBottomUp1e-1")
+# models+=("SimplifierBottomUp1e-2,SimplifierBottomUp1e-4")
+# models+=("SimplifierBottomUp1e-6,SimplifierBottomUp1e-10")
 
 for model in "${models[@]}"
 do
     # -repeats: runs per seed. -n_trials: number of different seeds
-    python submit_jobs.py -repeats 1 -n_trials 30 -models "$model" -n_jobs 60 -data-dir ./data/lexicase_paper --local # --slurm -time 1:45:00 -m 1000 # 
+    python submit_jobs.py -repeats 1 -n_trials 10 -models "$model" -n_jobs 60 -data-dir ./data/lexicase_paper --local # --slurm -time 1:45:00 -m 1000 # 
 done
