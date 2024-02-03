11b277a9d3b609c3603244640cc10a2172b559a4
gAldeia <guilherme.seidyo@gmail.com>
2024-02-02 19:18:13 -0500

    Updated to paper experiments

--- models/_default_kwargs.py
+++ models/_default_kwargs.py
@@ -1,8 +1,8 @@
 kwargs = {
     'pop_size'       : 80, 
     'max_gen'        : 201,
-    'max_depth'      : 6,
-    'max_size'       : 2**6,
+    'max_depth'      : 7,
+    'max_size'       : 2**7,
     'objectives'     : ['error', 'size'],
     'cx_prob'        : 1/(4+1), #1/(4+1), # n_mutations+1
     'initialization' : 'uniform',
@@ -10,7 +10,7 @@ kwargs = {
     'validation_size': 0.33,
     'verbosity'      : 0,
     'survival'       : 'tournament', #'nsga2', 'offspring'
-    'simplification_tolerance' : 1e-8,
+    'simplification_tolerance' : 1e-2,
     'functions'      : [
                         'div', 'add', 'sub', 'mul',
                         # 'add3', 'add4', 'mul3', 'mul4',
